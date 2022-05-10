bd6c7a1ca1ae6cb3dd0d14ce046196181a987f76
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2022-05-09 20:13:35 -0700

    Evolution chain and generations part of info page are done. Entry and habitats sections still need to be completed.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # Title: main.py
 # Description: Holds Flaks application code and all routes to web pages
 
+from ensurepip import version
+from info import preprocess_evolution, preprocess_versions
 import random
 import pokebase as pb
+import requests, json
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+from pprint import pprint
 from PIL import Image
 
 # creating flask appliation
@@ -20,9 +24,21 @@ def home():
 
 # Worked on by: Pedro
 # Pokemon info route
-@app.route('/info')
-def info():
-    return render_template('info.html')
+@app.route('/info/<name>')
+def info(name):
+    pokemon = pb.pokemon(name)
+    bio = pokemon.species.flavor_text_entries[0].__dict__['flavor_text']
+
+    try:
+        r = requests.get(pokemon.species.evolution_chain.url)
+        data = r.json()
+        evolution_dict = preprocess_evolution(data["chain"])
+    except:
+        print('error')
+
+    gen_dict = preprocess_versions(pokemon.sprites.versions.__dict__, pokemon.id)
+
+    return render_template('info.html', pokemon=pokemon, bio=bio, evolution_dict=evolution_dict, gen_dict=gen_dict)
   
 # Lists Pokemon types
 @app.route('/types')
