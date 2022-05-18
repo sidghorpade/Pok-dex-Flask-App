04cc71a73fa4e891692ad39bc03cdf4228146600
Siddhi ghorpade <92146463+sidghorpade@users.noreply.github.com>
2022-05-17 22:33:23 -0700

    cleaned up the code a little

--- main.py
+++ main.py
@@ -144,35 +144,9 @@ def selectedType(type):
     limit = len(poke_list)
     return render_template('selectedType.html', poke_list = poke_list, type = type, limit = limit)
 
-def validate_image(stream):
-    header = stream.read(512)
-    stream.seek(0)
-    format = imghdr.what(None, header)
-    if not format:
-        return None
-    return '.' + (format if format != 'jpeg' else 'jpg')
-
-@app.errorhandler(413)
-def too_large(e):
-    return "File is too large", 413
-
+# Search by image route
 @app.route('/searchByImage')
 def index():
     files = os.listdir(app.config['UPLOAD_PATH'])
     return render_template('searchByImage.html', files=files)
 
-@app.route('/searchByImage', methods=['POST'])
-def upload_files():
-    uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename)
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
-            return "Invalid image", 400
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    return '', 204
-
-@app.route('/uploads/<filename>')
-def upload(filename):
-    return send_from_directory(app.config['UPLOAD_PATH'], filename)
