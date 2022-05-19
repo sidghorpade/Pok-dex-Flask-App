f3bab0bef30726543537e4f32e76a8a34f7506b4
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2022-05-18 21:27:34 -0700

    made some of the pages look nicer with css

--- transformations.py
+++ transformations.py
@@ -10,7 +10,10 @@
 import base64
 from io import BytesIO
 
+# Apply a given image transformation to a given image
 def transform_image(img, transformation):
+    # Will perform transformation based on input
+    # If input is 'normal', do nothing
     if transformation == 'grayscale':
         img = grayscale_list(img)
     elif transformation == 'sepia':
@@ -20,17 +23,14 @@ def transform_image(img, transformation):
 
     return get_img(img)
 
+# Convert image so that it can be used in HTML
 def get_img(img):
     img_data = BytesIO()
     img.save(img_data, "PNG")
     img_str = base64.b64encode(img_data.getvalue()).decode('ascii')
     return f'data:image/png;base64,{ img_str }'
 
-def sepia_list(img):
-    sep_list = [sepia(i) for i in img.getdata()]
-    img.putdata(sep_list)
-    return img
-
+# Perform sepia image transformation for current pixel
 def sepia(i):
     # tint shadows
     if i[0] < 63:
@@ -47,23 +47,33 @@ def sepia(i):
     
     return r, g, b
 
+# Get list of sepia transformed pixels
+def sepia_list(img):
+    sep_list = [sepia(i) for i in img.getdata()]
+    img.putdata(sep_list)
+    return img
+
+# Perform negative image transformation for current pixel
 def negative(i):
     # taking negative of rgb values
     r,g,b = 255 - i[0], 255 - i[1], 255 - i[2]
 
     return r, g, b
 
+# Get list of negative transformed pixels
 def negative_list(img):
     neg_list = [negative(i) for i in img.getdata()]
     img.putdata(neg_list)
     return img
 
+# Perform grayscale image transformation for current pixel
 def grayscale(i):
     # performing grayscale conversion
     r,g,b = ((i[0]*299 + i[1]*587 + i[2]*114) // 1000,) * 3
 
     return r, g, b
 
+# Get list of grayscale transformed pixels
 def grayscale_list(img):
     gray_list = [grayscale(i) for i in img.getdata()]
     img.putdata(gray_list)
